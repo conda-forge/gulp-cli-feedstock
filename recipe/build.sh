3d3a4f0fb012a5e3e4c2916934a608c7632e177e
Uwe L. Korn <uwe.korn@quantco.com>
2024-03-25 08:27:01 +0000

    Switch to modern build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,22 @@
-yarn pack
-yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export npm_config_arch="arm64"
+fi
+
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
+
+rm $PREFIX/bin/node
+ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
-npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+
+# No lockfile present
+# pnpm import
+pnpm install
+pnpm pack
+npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
